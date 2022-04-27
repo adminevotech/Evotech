9f94a44db41b89a1ecf9c7aececef112767e7447
Hussien EvoTech <h.ali@evotech-co.com>
2022-04-27 09:15:19 +0200

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(StaticContentSeeder::class);
+        $this->call(SuperAdminSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
     }
 }
