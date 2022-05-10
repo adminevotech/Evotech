6f823f911a088f27fbaef839c46cf96f86dec5d4
Hussien EvoTech <h.ali@evotech-co.com>
2022-05-10 10:47:23 +0200

    portfolio routes

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Portfolio extends Model
+class Portfolio extends Model implements HasMedia
 {
     use HasFactory, SoftDeletes, LogsActivity, InteractsWithMedia;
     protected $fillable = ['name', 'link', 'service_id', 'active'];
